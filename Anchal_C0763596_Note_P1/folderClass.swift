b2fd44071506512140e7fde3d609ec09807664a4
Anchal <anchalrajput43@gmail.com>
2019-11-08 17:06:24 -0500

    change

--- Anchal_C0763596_Note_P1/folderClass.swift
+++ Anchal_C0763596_Note_P1/folderClass.swift
@@ -18,6 +18,8 @@ class folderClass: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+       // folders = []
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
